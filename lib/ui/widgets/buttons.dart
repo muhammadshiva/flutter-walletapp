ea13785e269d5268f36d4bed515f7b5169da3052
muhammadshiva <shivamuhammad07@gmail.com>
2022-10-06 00:32:43 +0700

    create custom widget

---
+++ lib/ui/widgets/buttons.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import '../../shared/theme.dart';
+
+class CustomFilledButton extends StatelessWidget {
+  const CustomFilledButton({
+    Key? key,
+    required this.title,
+    this.width = double.infinity,
+    this.height = 50,
+    this.onPressed,
+  }) : super(key: key);
+
+  final String title;
+  final double width;
+  final double height;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          backgroundColor: purpleColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(56),
+          ),
+        ),
+        child: Text(
+          title,
+          style: whiteTextStyle.copyWith(
+            fontWeight: semiBold,
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextWidget extends StatelessWidget {
+  const CustomTextWidget({
+    Key? key,
+    required this.title,
+    this.width = double.infinity,
+    this.height = 24,
+    this.onPressed,
+  }) : super(key: key);
+
+  final String title;
+  final double width;
+  final double height;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.zero,
+        ),
+        child: Text(
+          title,
+          style: greyTextStyle.copyWith(
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}
