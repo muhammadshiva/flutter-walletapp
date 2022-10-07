715eaa77b2d8b9cd2e2df564796a989a9aa63b54
muhammadshiva <shivamuhammad07@gmail.com>
2022-10-07 12:59:23 +0700

    (home page): add navigation bar

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:moneywise_app/shared/theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: lightBgColor,
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: blueColor,
+        unselectedItemColor: blackColor,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        selectedLabelStyle: blueTextStyle.copyWith(
+          fontSize: 10,
+          fontWeight: medium,
+        ),
+        unselectedLabelStyle: blackTextStyle.copyWith(
+          fontSize: 10,
+          fontWeight: medium,
+        ),
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/ic_overview.png',
+              width: 20,
+              height: 20,
+              color: blueColor,
+            ),
+            label: 'Overview',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/ic_history.png',
+              width: 20,
+              height: 20,
+            ),
+            label: 'History',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/ic_statistic.png',
+              width: 20,
+              height: 20,
+            ),
+            label: 'Statistic',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/ic_reward.png',
+              width: 20,
+              height: 20,
+            ),
+            label: 'Reward',
+          ),
+        ],
+      ),
+    );
   }
 }
