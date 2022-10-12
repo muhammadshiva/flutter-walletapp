738bb0d3a4b3e1c9dba0da4cbc6fb850f3a8ccf0
muhammadshiva <shivamuhammad07@gmail.com>
2022-10-12 15:17:38 +0700

    add navigation with result

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:moneywise_app/shared/theme.dart';
 import 'package:moneywise_app/ui/pages/home_page.dart';
 import 'package:moneywise_app/ui/pages/onboarding_page.dart';
 import 'package:moneywise_app/ui/pages/pin_page.dart';
+import 'package:moneywise_app/ui/pages/profile_edit_page.dart';
 import 'package:moneywise_app/ui/pages/profile_page.dart';
 import 'package:moneywise_app/ui/pages/sign_in_page.dart';
 import 'package:moneywise_app/ui/pages/sign_up_page.dart';
@@ -47,6 +48,7 @@ class MyApp extends StatelessWidget {
         '/home': (context) => const HomePage(),
         '/profile': (context) => const ProfilePage(),
         '/pin': (context) => const PinPage(),
+        '/profile-edit': (context) => const ProfileEditPage(),
       },
     );
   }
