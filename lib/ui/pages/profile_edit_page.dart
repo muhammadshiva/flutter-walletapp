14c69b56044c7b09354e2f9ad264116c3ec24e0d
muhammadshiva <shivamuhammad07@gmail.com>
2022-10-12 15:25:02 +0700

    (edit profile page) : slicing page

--- lib/ui/pages/profile_edit_page.dart
+++ lib/ui/pages/profile_edit_page.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
 
+import '../../shared/theme.dart';
+import '../widgets/buttons.dart';
+import '../widgets/forms.dart';
+
 class ProfileEditPage extends StatelessWidget {
   const ProfileEditPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Photo'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        children: [
+          const SizedBox(height: 30),
+          Container(
+            padding: const EdgeInsets.all(22),
+            decoration: BoxDecoration(
+              color: whiteColor,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CustomFormField(title: 'Username'),
+                const SizedBox(height: 16),
+                const CustomFormField(title: 'Full Name'),
+                const SizedBox(height: 16),
+                const CustomFormField(title: 'Email Address'),
+                const SizedBox(height: 16),
+                const CustomFormField(
+                  title: 'Password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 30),
+                CustomFilledButton(
+                  title: 'Update Now',
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/sign-up-set-profile');
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
