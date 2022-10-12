b9ca842e326a0f818606f4034101e7133c11100f
muhammadshiva <shivamuhammad07@gmail.com>
2022-10-12 08:39:01 +0700

    (home page) : build friendly tips wigets 1

---
+++ lib/ui/widgets/home_tips_item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:moneywise_app/shared/theme.dart';
+
+class HomeTipsItem extends StatelessWidget {
+  const HomeTipsItem({
+    Key? key,
+    required this.imageUrl,
+    required this.title,
+    required this.url,
+  }) : super(key: key);
+
+  final String imageUrl;
+  final String title;
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 155,
+      height: 176,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: whiteColor,
+      ),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.vertical(
+              top: Radius.circular(20),
+            ),
+            child: Image.asset(
+              imageUrl,
+              width: 155,
+              height: 110,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(height: 12),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Text(
+              title,
+              style: blackTextStyle.copyWith(
+                fontWeight: medium,
+              ),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
