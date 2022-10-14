7fad5ba815565d65a4d218d4e90a22b0598c7fb3
muhammadshiva <shivamuhammad07@gmail.com>
2022-10-14 23:22:27 +0700

    (logout) : add bloc consumer

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:moneywise_app/blocs/auth/auth_bloc.dart';
+import 'package:moneywise_app/shared/shared_methods.dart';
 import 'package:moneywise_app/shared/theme.dart';
 import 'package:moneywise_app/ui/widgets/buttons.dart';
 import 'package:moneywise_app/ui/widgets/profile_menu_item.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -14,101 +17,125 @@ class ProfilePage extends StatelessWidget {
           'My Profile',
         ),
       ),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        children: [
-          const SizedBox(height: 30),
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 30,
-              vertical: 22,
-            ),
-            decoration: BoxDecoration(
-              color: whiteColor,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Column(
-              children: [
-                Container(
-                  width: 120,
-                  height: 120,
-                  margin: const EdgeInsets.only(bottom: 16),
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: AssetImage('assets/img_profile.png'),
-                    ),
-                  ),
-                  child: Align(
-                    alignment: Alignment.topRight,
-                    child: Container(
-                      width: 28,
-                      height: 28,
-                      decoration: BoxDecoration(
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthFailed) {
+            showCustomSnackbar(context, state.e);
+          }
+
+          if (state is AuthInitial) {
+            Navigator.pushNamedAndRemoveUntil(
+                context, '/sign-in', (route) => false);
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            children: [
+              const SizedBox(height: 30),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                  vertical: 22,
+                ),
+                decoration: BoxDecoration(
+                  color: whiteColor,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      width: 120,
+                      height: 120,
+                      margin: const EdgeInsets.only(bottom: 16),
+                      decoration: const BoxDecoration(
                         shape: BoxShape.circle,
-                        color: whiteColor,
+                        image: DecorationImage(
+                          image: AssetImage('assets/img_profile.png'),
+                        ),
                       ),
-                      child: Center(
-                        child: Icon(
-                          Icons.check_circle,
-                          color: greenColor,
-                          size: 24,
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Container(
+                          width: 28,
+                          height: 28,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: whiteColor,
+                          ),
+                          child: Center(
+                            child: Icon(
+                              Icons.check_circle,
+                              color: greenColor,
+                              size: 24,
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
+                    Text(
+                      'Shayna Hanna',
+                      style: blackTextStyle.copyWith(
+                        fontWeight: medium,
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                    ProfileMenuItem(
+                      iconUrl: 'assets/ic_edit_profile.png',
+                      title: 'Edit Profile',
+                      onTap: () async {
+                        if (await Navigator.pushNamed(context, '/pin') ==
+                            true) {
+                          Navigator.pushNamed(context, '/profile-edit');
+                        }
+                      },
+                    ),
+                    ProfileMenuItem(
+                      iconUrl: 'assets/ic_pin.png',
+                      title: 'My Pin',
+                      onTap: () async {
+                        if (await Navigator.pushNamed(context, '/pin') ==
+                            true) {
+                          Navigator.pushNamed(context, '/profile-edit-pin');
+                        }
+                      },
+                    ),
+                    ProfileMenuItem(
+                      iconUrl: 'assets/ic_wallet.png',
+                      title: 'Wallet Settings',
+                      onTap: () {},
+                    ),
+                    ProfileMenuItem(
+                      iconUrl: 'assets/ic_help.png',
+                      title: 'Help Center',
+                      onTap: () {},
+                    ),
+                    ProfileMenuItem(
+                      iconUrl: 'assets/ic_logout.png',
+                      title: 'Log Out',
+                      onTap: () {
+                        context.read<AuthBloc>().add(AuthLogout());
+                      },
+                    ),
+                  ],
                 ),
-                Text(
-                  'Shayna Hanna',
-                  style: blackTextStyle.copyWith(
-                    fontWeight: medium,
-                    fontSize: 18,
-                  ),
-                ),
-                const SizedBox(height: 40),
-                ProfileMenuItem(
-                  iconUrl: 'assets/ic_edit_profile.png',
-                  title: 'Edit Profile',
-                  onTap: () async {
-                    if (await Navigator.pushNamed(context, '/pin') == true) {
-                      Navigator.pushNamed(context, '/profile-edit');
-                    }
-                  },
-                ),
-                ProfileMenuItem(
-                  iconUrl: 'assets/ic_pin.png',
-                  title: 'My Pin',
-                  onTap: () async {
-                    if (await Navigator.pushNamed(context, '/pin') == true) {
-                      Navigator.pushNamed(context, '/profile-edit-pin');
-                    }
-                  },
-                ),
-                ProfileMenuItem(
-                  iconUrl: 'assets/ic_wallet.png',
-                  title: 'Wallet Settings',
-                  onTap: () {},
-                ),
-                ProfileMenuItem(
-                  iconUrl: 'assets/ic_help.png',
-                  title: 'Help Center',
-                  onTap: () {},
-                ),
-                ProfileMenuItem(
-                  iconUrl: 'assets/ic_logout.png',
-                  title: 'Edit Profile',
-                  onTap: () {},
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 87),
-          CustomTextButton(
-            title: 'Report a Problem',
-            onPressed: () {},
-          ),
-          const SizedBox(height: 50),
-        ],
+              ),
+              const SizedBox(height: 87),
+              CustomTextButton(
+                title: 'Report a Problem',
+                onPressed: () {},
+              ),
+              const SizedBox(height: 50),
+            ],
+          );
+        },
       ),
     );
   }
