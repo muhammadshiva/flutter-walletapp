c2e9b0f80143149485f9b53a5f183baa40b270fc
muhammadshiva <shivamuhammad07@gmail.com>
2022-10-14 22:07:23 +0700

    (pin) : create bloc

--- lib/blocs/auth/auth_bloc.dart
+++ lib/blocs/auth/auth_bloc.dart
@@ -5,6 +5,7 @@ import 'package:moneywise_app/models/user_edit_form_model.dart';
 import 'package:moneywise_app/models/user_model.dart';
 import 'package:moneywise_app/services/auth_service.dart';
 import 'package:moneywise_app/services/user_service.dart';
+import 'package:moneywise_app/services/wallet_service.dart';
 
 import '../../models/signin_form_model.dart';
 
@@ -83,6 +84,24 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
               emit(AuthLoading());
 
               await UserService().updateUser(event.data);
+
+              emit(AuthSuccess(updatedUser));
+            }
+          } catch (e) {
+            emit(AuthFailed(e.toString()));
+          }
+        }
+
+        if (event is AuthUpdatePin) {
+          try {
+            if (state is AuthSuccess) {
+              final updatedUser = (state as AuthSuccess).user.copyWith(
+                    pin: event.newPin,
+                  );
+
+              emit(AuthLoading());
+
+              await WalletService().updatePin(event.oldPin, event.newPin);
 
               emit(AuthSuccess(updatedUser));
             }
