ab1dba882b8ca4805f95acd9d99981a65ab8f26b
muhammadshiva <shivamuhammad07@gmail.com>
2022-10-14 14:03:12 +0700

    (login) : create event

--- lib/blocs/auth/auth_event.dart
+++ lib/blocs/auth/auth_event.dart
@@ -22,3 +22,10 @@ class AuthRegister extends AuthEvent {
   @override
   List<Object> get props => [data];
 }
+
+class AuthLogin extends AuthEvent {
+  final SignInFormModel data;
+  const AuthLogin(this.data);
+  @override
+  List<Object> get props => [data];
+}
