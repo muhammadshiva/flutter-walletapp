d09d0f88230891ffbd5f4756811087e9141d2b53
muhammadshiva <shivamuhammad07@gmail.com>
2022-10-15 15:12:26 +0700

    (top up) : button improvement

--- lib/ui/pages/topup_page.dart
+++ lib/ui/pages/topup_page.dart
@@ -115,27 +115,31 @@ class _TopupPageState extends State<TopupPage> {
                 ),
               ),
               const SizedBox(height: 30),
-              if (selectedPaymentMethod != null)
-                CustomFilledButton(
-                  title: 'Continue',
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => TopupAmountPage(
-                          data: TopupFormModel(
-                            paymentMethodCode: selectedPaymentMethod?.code,
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              const SizedBox(height: 57),
             ],
           ),
         ],
       ),
+      floatingActionButton: (selectedPaymentMethod != null)
+          ? Container(
+              margin: const EdgeInsets.all(24),
+              child: CustomFilledButton(
+                title: 'Continue',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => TopupAmountPage(
+                        data: TopupFormModel(
+                          paymentMethodCode: selectedPaymentMethod?.code,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
+          : Container(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
