17e3090cd1c74c9157065c2d21f39935c141b859
muhammadshiva <shivamuhammad07@gmail.com>
2022-10-15 12:27:37 +0700

    (top up) : implement bloc top up

--- lib/ui/pages/topup_amount_page.dart
+++ lib/ui/pages/topup_amount_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
+import 'package:moneywise_app/blocs/auth/auth_bloc.dart';
 import 'package:moneywise_app/models/topup_form_model.dart';
+import 'package:moneywise_app/shared/shared_methods.dart';
 import 'package:moneywise_app/shared/theme.dart';
 import 'package:moneywise_app/ui/widgets/buttons.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+import '../../blocs/topup/topup_bloc.dart';
 
 class TopupAmountPage extends StatefulWidget {
   final TopupFormModel data;
@@ -67,163 +72,194 @@ class _TopupAmountPageState extends State<TopupAmountPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: darkBgColor,
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 58),
-        children: [
-          const SizedBox(height: 60),
-          Center(
-            child: Text(
-              'Total Amount',
-              style: whiteTextStyle.copyWith(
-                fontWeight: semiBold,
-                fontSize: 20,
-              ),
-            ),
-          ),
-          const SizedBox(height: 67),
-          Align(
-            alignment: Alignment.center,
-            child: SizedBox(
-              width: 200,
-              child: TextFormField(
-                controller: amountController,
-                cursorColor: greyColor,
-                enabled: false,
-                style: whiteTextStyle.copyWith(
-                  fontSize: 36,
-                  fontWeight: medium,
-                ),
-                decoration: InputDecoration(
-                  prefixIcon: Text(
-                    'Rp ',
-                    style: whiteTextStyle.copyWith(
-                      fontSize: 36,
-                      fontWeight: medium,
-                    ),
-                  ),
-                  disabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: greyColor,
-                    ),
-                  ),
-                  // focusedBorder: UnderlineInputBorder(
-                  //   borderSide: BorderSide(
-                  //     color: greyColor,
-                  //   ),
-                  // ),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 66),
-          Wrap(
-            spacing: 40,
-            runSpacing: 40,
-            children: [
-              CustomInputButton(
-                title: '1',
-                onTap: () {
-                  addAmount('1');
-                },
-              ),
-              CustomInputButton(
-                title: '2',
-                onTap: () {
-                  addAmount('2');
-                },
-              ),
-              CustomInputButton(
-                title: '3',
-                onTap: () {
-                  addAmount('3');
-                },
-              ),
-              CustomInputButton(
-                title: '4',
-                onTap: () {
-                  addAmount('4');
-                },
-              ),
-              CustomInputButton(
-                title: '5',
-                onTap: () {
-                  addAmount('5');
-                },
-              ),
-              CustomInputButton(
-                title: '6',
-                onTap: () {
-                  addAmount('6');
-                },
-              ),
-              CustomInputButton(
-                title: '7',
-                onTap: () {
-                  addAmount('7');
-                },
-              ),
-              CustomInputButton(
-                title: '8',
-                onTap: () {
-                  addAmount('8');
-                },
-              ),
-              CustomInputButton(
-                title: '9',
-                onTap: () {
-                  addAmount('9');
-                },
-              ),
-              const SizedBox(
-                height: 60,
-                width: 60,
-              ),
-              CustomInputButton(
-                title: '0',
-                onTap: () {
-                  addAmount('0');
-                },
-              ),
-              GestureDetector(
-                onTap: () {
-                  deleteAmount();
-                },
-                child: Container(
-                  width: 60,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: numberBgColor,
-                  ),
-                  child: Center(
-                    child: Icon(
-                      Icons.arrow_back,
-                      color: whiteColor,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 50),
-          CustomFilledButton(
-            title: 'Checkout Now',
-            onPressed: () async {
-              if (await Navigator.pushNamed(context, '/pin') == true) {
-                await launchUrl(Uri.parse('https://demo.midtrans.com/'));
+      body: BlocProvider(
+        create: (context) => TopupBloc(),
+        child: BlocConsumer<TopupBloc, TopupState>(
+          listener: (context, state) async {
+            if (state is TopupFailed) {
+              showCustomSnackbar(context, state.e);
+            }
 
-                Navigator.pushNamedAndRemoveUntil(
-                    context, '/topup-success', (route) => false);
-              }
-            },
-          ),
-          const SizedBox(height: 25),
-          CustomTextButton(
-            title: 'Terms & Condition',
-            onPressed: () {},
-          ),
-          const SizedBox(height: 40),
-        ],
+            if (state is TopupSuccess) {
+              await launchUrl(Uri.parse(state.redirectUrl));
+
+              Navigator.pushNamedAndRemoveUntil(
+                  context, '/topup-success', (route) => false);
+            }
+          },
+          builder: (context, state) {
+            return ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 58),
+              children: [
+                const SizedBox(height: 60),
+                Center(
+                  child: Text(
+                    'Total Amount',
+                    style: whiteTextStyle.copyWith(
+                      fontWeight: semiBold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 67),
+                Align(
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    width: 200,
+                    child: TextFormField(
+                      controller: amountController,
+                      cursorColor: greyColor,
+                      enabled: false,
+                      style: whiteTextStyle.copyWith(
+                        fontSize: 36,
+                        fontWeight: medium,
+                      ),
+                      decoration: InputDecoration(
+                        prefixIcon: Text(
+                          'Rp ',
+                          style: whiteTextStyle.copyWith(
+                            fontSize: 36,
+                            fontWeight: medium,
+                          ),
+                        ),
+                        disabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: greyColor,
+                          ),
+                        ),
+                        // focusedBorder: UnderlineInputBorder(
+                        //   borderSide: BorderSide(
+                        //     color: greyColor,
+                        //   ),
+                        // ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 66),
+                Wrap(
+                  spacing: 40,
+                  runSpacing: 40,
+                  children: [
+                    CustomInputButton(
+                      title: '1',
+                      onTap: () {
+                        addAmount('1');
+                      },
+                    ),
+                    CustomInputButton(
+                      title: '2',
+                      onTap: () {
+                        addAmount('2');
+                      },
+                    ),
+                    CustomInputButton(
+                      title: '3',
+                      onTap: () {
+                        addAmount('3');
+                      },
+                    ),
+                    CustomInputButton(
+                      title: '4',
+                      onTap: () {
+                        addAmount('4');
+                      },
+                    ),
+                    CustomInputButton(
+                      title: '5',
+                      onTap: () {
+                        addAmount('5');
+                      },
+                    ),
+                    CustomInputButton(
+                      title: '6',
+                      onTap: () {
+                        addAmount('6');
+                      },
+                    ),
+                    CustomInputButton(
+                      title: '7',
+                      onTap: () {
+                        addAmount('7');
+                      },
+                    ),
+                    CustomInputButton(
+                      title: '8',
+                      onTap: () {
+                        addAmount('8');
+                      },
+                    ),
+                    CustomInputButton(
+                      title: '9',
+                      onTap: () {
+                        addAmount('9');
+                      },
+                    ),
+                    const SizedBox(
+                      height: 60,
+                      width: 60,
+                    ),
+                    CustomInputButton(
+                      title: '0',
+                      onTap: () {
+                        addAmount('0');
+                      },
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        deleteAmount();
+                      },
+                      child: Container(
+                        width: 60,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: numberBgColor,
+                        ),
+                        child: Center(
+                          child: Icon(
+                            Icons.arrow_back,
+                            color: whiteColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 50),
+                CustomFilledButton(
+                  title: 'Checkout Now',
+                  onPressed: () async {
+                    if (await Navigator.pushNamed(context, '/pin') == true) {
+                      final authState = context.read<AuthBloc>().state;
+
+                      String pin = '';
+                      if (authState is AuthSuccess) {
+                        pin = authState.user.pin!;
+                      }
+
+                      context.read<TopupBloc>().add(
+                            TopupPost(
+                              widget.data.copyWith(
+                                pin: pin,
+                                amount:
+                                    amountController.text.replaceAll('.', ''),
+                              ),
+                            ),
+                          );
+                    }
+                  },
+                ),
+                const SizedBox(height: 25),
+                CustomTextButton(
+                  title: 'Terms & Condition',
+                  onPressed: () {},
+                ),
+                const SizedBox(height: 40),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
