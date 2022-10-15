9c21d175c692e55fd4a14f5c923778c7f88c9075
muhammadshiva <shivamuhammad07@gmail.com>
2022-10-15 23:32:19 +0700

    updata wallet data

--- lib/ui/pages/data_provider_page.dart
+++ lib/ui/pages/data_provider_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moneywise_app/shared/theme.dart';
 import 'package:moneywise_app/ui/widgets/buttons.dart';
 import 'package:moneywise_app/ui/widgets/data_provider_item.dart';
 
+import '../../blocs/auth/auth_bloc.dart';
 import '../../shared/shared_methods.dart';
 
 class DataProviderPage extends StatelessWidget {
@@ -33,20 +35,28 @@ class DataProviderPage extends StatelessWidget {
                 width: 80,
               ),
               const SizedBox(width: 16),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '8008 2208 1996',
-                    style: blackTextStyle.copyWith(
-                        fontWeight: medium, fontSize: 16),
-                  ),
-                  const SizedBox(height: 2),
-                  Text(
-                    'Balance: ${formatCurrency(180000000)}',
-                    style: greyTextStyle.copyWith(fontSize: 12),
-                  ),
-                ],
+              BlocBuilder<AuthBloc, AuthState>(
+                builder: (context, state) {
+                  if (state is AuthSuccess) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          state.user.cardNumber!.replaceAllMapped(
+                              RegExp(r".{4}"), (match) => "${match.group(0)} "),
+                          style: blackTextStyle.copyWith(
+                              fontWeight: medium, fontSize: 16),
+                        ),
+                        const SizedBox(height: 2),
+                        Text(
+                          'Balance: ${formatCurrency(state.user.balance ?? 0)}',
+                          style: greyTextStyle.copyWith(fontSize: 12),
+                        ),
+                      ],
+                    );
+                  }
+                  return Container();
+                },
               )
             ],
           ),
