619b32af8a293a22a8fba675df072b7895ff1881
muhammadshiva <shivamuhammad07@gmail.com>
2022-10-16 08:27:42 +0700

    (data plan) : add service API

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -5,6 +5,7 @@ import 'package:moneywise_app/models/topup_form_model.dart';
 import 'package:moneywise_app/services/auth_service.dart';
 import 'package:moneywise_app/shared/shared_values.dart';
 
+import '../models/data_form_model.dart';
 import '../models/transfer_form_model.dart';
 
 class TransactionService {
@@ -39,6 +40,28 @@ class TransactionService {
       final res = await http.post(
         Uri.parse(
           '$baseUrl/transfers',
+        ),
+        headers: {
+          'Authorization': token,
+        },
+        body: data.toJson(),
+      );
+
+      if (res.statusCode != 200) {
+        throw jsonDecode(res.body)['message'];
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> dataPlan(DataPlanFormModel data) async {
+    try {
+      final token = await AuthService().getToken();
+
+      final res = await http.post(
+        Uri.parse(
+          '$baseUrl/data_plans',
         ),
         headers: {
           'Authorization': token,
