9f8f4f62462e75e8b3b6824063eeeb6a035fd27a
muhammadshiva <shivamuhammad07@gmail.com>
2022-11-17 09:30:22 +0700

    update shared values

--- lib/shared/shared_values.dart
+++ lib/shared/shared_values.dart
@@ -1,4 +1,4 @@
 //String baseUrl = 'https://bwabank.my.id/api';
 // String baseUrl = 'http://money-wise-backend.test/api';
 // String baseUrl = 'http://192.168.1.4:8000/api';
-String baseUrl = 'http://192.168.8.115:8000/api';
+String baseUrl = 'https://money-wise-backend.sharedwithexpose.com/api';
