ac355d0f6055af598ff54f9aa7dbc4aba1a5464b
muhammadshiva <shivamuhammad07@gmail.com>
2022-12-06 22:40:09 +0700

    delete payment method code

--- lib/models/transaction_model.dart
+++ lib/models/transaction_model.dart
@@ -6,14 +6,14 @@ class TransactionModel {
   final int? id;
   final int? amount;
   final DateTime? createdAt;
-  final PaymentMethodModel? paymentMethod;
+  // final PaymentMethodModel? paymentMethod;
   final TransactionTypeModel? transactionType;
 
   TransactionModel(
       {this.id,
       this.amount,
       this.createdAt,
-      this.paymentMethod,
+      // this.paymentMethod,
       this.transactionType});
 
   factory TransactionModel.fromJson(Map<String, dynamic> json) =>
@@ -21,9 +21,9 @@ class TransactionModel {
         id: json['id'],
         amount: json['amount'],
         createdAt: DateTime.tryParse(json['created_at']),
-        paymentMethod: json['payment_method'] == null
-            ? null
-            : PaymentMethodModel.fromJson(json['payment_method']),
+        // paymentMethod: json['payment_method'] == null
+        //     ? null
+        //     : PaymentMethodModel.fromJson(json['payment_method']),
         transactionType: json['transaction_type'] == null
             ? null
             : TransactionTypeModel.fromJson(json['transaction_type']),
