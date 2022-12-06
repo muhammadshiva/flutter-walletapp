409f72255d5174e412ad61a41998fbd594da809d
muhammadshiva <shivamuhammad07@gmail.com>
2022-12-06 22:39:54 +0700

    delete payment method code

--- lib/models/topup_form_model.dart
+++ lib/models/topup_form_model.dart
@@ -1,12 +1,12 @@
 class TopupFormModel {
   final String? amount;
   final String? pin;
-  final String? paymentMethodCode;
+  // final String? paymentMethodCode;
 
   TopupFormModel({
     this.amount,
     this.pin,
-    this.paymentMethodCode,
+    // this.paymentMethodCode,
   });
 
   TopupFormModel copyWith({
@@ -17,12 +17,12 @@ class TopupFormModel {
       TopupFormModel(
         amount: amount ?? this.amount,
         pin: pin ?? this.pin,
-        paymentMethodCode: paymentMethodCode ?? this.paymentMethodCode,
+        // paymentMethodCode: paymentMethodCode ?? this.paymentMethodCode,
       );
 
   Map<String, dynamic> toJson() => {
         'amount': amount,
         'pin': pin,
-        'payment_method_code': paymentMethodCode,
+        // 'payment_method_code': paymentMethodCode,
       };
 }
