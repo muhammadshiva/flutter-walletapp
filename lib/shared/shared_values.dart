3394183ebf92d02281fe8ec9a21cec0875115747
muhammadshiva <shivamuhammad07@gmail.com>
2022-12-06 22:46:55 +0700

    add some values

--- lib/shared/shared_values.dart
+++ lib/shared/shared_values.dart
@@ -1,4 +1,13 @@
 //String baseUrl = 'https://bwabank.my.id/api';
 // String baseUrl = 'http://money-wise-backend.test/api';
 // String baseUrl = 'http://192.168.1.4:8000/api';
-String baseUrl = 'https://money-wise-backend.sharedwithexpose.com/api';
+
+//String baseUrl = 'https://money-wise-backend.sharedwithexpose.com/api/v1';
+
+// String baseUrl = 'http://192.168.1.7:8000/api/v1';
+
+String baseUrl =
+    'https://6bde-2001-448a-50a0-58bd-f949-5b68-9864-2e7d.ap.ngrok.io/api/v1';
+
+String baseImageUrl =
+    'https://6bde-2001-448a-50a0-58bd-f949-5b68-9864-2e7d.ap.ngrok.io/';
