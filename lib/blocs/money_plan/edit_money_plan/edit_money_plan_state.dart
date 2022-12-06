3a3073e5359a5fa60465ad9f93e1dda8abb98f1f
muhammadshiva <shivamuhammad07@gmail.com>
2022-12-06 22:26:44 +0700

    (edit money plan) : create state

---
+++ lib/blocs/money_plan/edit_money_plan/edit_money_plan_state.dart
@@ -0,0 +1,10 @@
+part of 'edit_money_plan_bloc.dart';
+
+abstract class EditMoneyPlanState extends Equatable {
+  const EditMoneyPlanState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+class EditMoneyPlanInitial extends EditMoneyPlanState {}
